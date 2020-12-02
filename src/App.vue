02ae42da44108370db61a0d88402290ba269fa69
cyz1901 <1093491992@qq.com>
2020-12-03 00:25:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,149 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      <v-treeview
+        v-model="tree"
+        :open="initiallyOpen"
+        :items="items"
+        activatable
+        item-key="name"
+        open-on-click
+      >
+        <template v-slot:prepend="{ item, open }">
+          <v-icon v-if="!item.file">
+            {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+          </v-icon>
+          <v-icon v-else>
+            {{ files[item.file] }}
+          </v-icon>
+        </template>
+      </v-treeview>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar><!-- 根据应用程序组件调整内容大小 --><v-main><!-- 为应用程序提供正确的插槽 --><v-container fluid><!-- 如果使用vue-router --><router-view></router-view>
+  </v-container>
+  </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+<!--  <v-app>-->
+<!--    <v-app-bar-->
+<!--      app-->
+<!--      color="primary"-->
+<!--      dark-->
+<!--    >-->
+<!--      <div class="d-flex align-center">-->
+<!--        <v-img-->
+<!--          alt="Vuetify Logo"-->
+<!--          class="shrink mr-2"-->
+<!--          contain-->
+<!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"-->
+<!--          transition="scale-transition"-->
+<!--          width="40"-->
+<!--        />-->
+
+<!--        <v-img-->
+<!--          alt="Vuetify Name"-->
+<!--          class="shrink mt-1 hidden-sm-and-down"-->
+<!--          contain-->
+<!--          min-width="100"-->
+<!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"-->
+<!--          width="100"-->
+<!--        />-->
+<!--      </div>-->
+
+<!--      <v-spacer></v-spacer>-->
+
+<!--      <v-btn-->
+<!--        href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
+<!--        target="_blank"-->
+<!--        text-->
+<!--      >-->
+<!--        <span class="mr-2">Latest Release</span>-->
+<!--        <v-icon>mdi-open-in-new</v-icon>-->
+<!--      </v-btn>-->
+<!--    </v-app-bar>-->
+
+<!--    <v-main>-->
+<!--      <router-view/>-->
+<!--    </v-main>-->
+<!--  </v-app>-->
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    initiallyOpen: ['public'],
+    files: {
+      html: 'mdi-language-html5',
+      js: 'mdi-nodejs',
+      json: 'mdi-code-json',
+      md: 'mdi-language-markdown',
+      pdf: 'mdi-file-pdf',
+      png: 'mdi-file-image',
+      txt: 'mdi-file-document-outline',
+      xls: 'mdi-file-excel',
+    },
+    tree: [],
+    items: [
+      {
+        name: '.git',
+      },
+      {
+        name: 'node_modules',
+      },
+      {
+        name: 'public',
+        children: [
+          {
+            name: 'static',
+            children: [{
+              name: 'logo.png',
+              file: 'png',
+            }],
+          },
+          {
+            name: 'favicon.ico',
+            file: 'png',
+          },
+          {
+            name: 'index.html',
+            file: 'html',
+          },
+        ],
+      },
+      {
+        name: '.gitignore',
+        file: 'txt',
+      },
+      {
+        name: 'babel.config.js',
+        file: 'js',
+      },
+      {
+        name: 'package.json',
+        file: 'json',
+      },
+      {
+        name: 'README.md',
+        file: 'md',
+      },
+      {
+        name: 'vue.config.js',
+        file: 'js',
+      },
+      {
+        name: 'yarn.lock',
+        file: 'txt',
+      },
+    ],
+  }),
+};
+</script>
